b4993d40142a9cd6d842e99ea7c78c7357cd2df8
sajakshrestha8 <shresthasajak123@gmail.com>
2024-11-08 10:23:42 +0545

    Updating many things

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let newWindow;
   let input = document.getElementById("input");
   let button = document.getElementById("button");
+  let addTask = document.getElementById("addtask");
 
   input.addEventListener("change", (e) => {
-    console.log(e.target.value); // Corrected here
+    console.log(e.target.value);
   });
 
   button.addEventListener("click", () => {
-    console.log("Button clicked");
+    newWindow = window.open("https://www.w3schools.com");
+  });
+
+  addTask.addEventListener("click", () => {
+    window.location.href = "AddTask.html";
   });
 });
