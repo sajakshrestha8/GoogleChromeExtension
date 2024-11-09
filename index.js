c89aca8ac853b9d14d2cea69cff2a8c9c1fbcae7
sajakshrestha8 <shresthasajak123@gmail.com>
2024-11-09 08:50:55 +0545

    testing

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let newWindow;
-  let input = document.getElementById("input");
-  let button = document.getElementById("button");
   let addTask = document.getElementById("addtask");
-
-  input.addEventListener("change", (e) => {
-    console.log(e.target.value);
-  });
-
-  button.addEventListener("click", () => {
-    newWindow = window.open("https://www.w3schools.com");
-  });
 
   addTask.addEventListener("click", () => {
     window.location.href = "AddTask.html";
